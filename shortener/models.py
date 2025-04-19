bf3dfd857b19ccf1f169fe276b43a1284b825524
Aliasgharcoder <aliasgharjutt611@gmail.com>
2025-04-20 03:23:56 +0500

    Add URL model with short code generator

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+def generate_short_code(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
+
+class ShortURL(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=15, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = generate_short_code()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.original_url} -> {self.short_code}"
